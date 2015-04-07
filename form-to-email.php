201e17469cec944289c28ab6e634515fce33390c
Ravi Kiran <raak@hackerstrip.com>
2015-04-07 05:49:25 +0530

    form-to-email.php created
    form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -1,15 +1,5 @@
-
 <?php
-  $name = $_POST['name'];
-  
-  $visitor_email = $_POST['email'];
-  $message = $_POST['message'];
-
-    $email_from = 'yourname@yourwebsite.com';
-
-    $email_subject = "New Form submission";
-
-    $email_body = "You have received a new message from the user $name.\n".
-                            "Here is the message:\n $message"
-
-    ?>
+if($_POST["message"]) {
+    mail("your@email.address", "Form to email message", $_POST["message"], "From: an@email.address");
+}
+?>
